d11530981e22982bdd965a26f55a77a8db86adb3
Matthew Stuart-Edwards <matthew.stuartedward@ucalgary.ca>
2024-10-10 12:55:40 -0600

    Refreshing setup script to run on clean cluster

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -4,4 +4,8 @@ kubectl create namespace graphkb
 kubectl create namespace ipr
 kubectl create namespace security
 kubectl create namespace db
-kubectl apply -f (string join ',' *.yaml)
+#kubectl apply -f (string join ',' *.yaml)
+minikube addons enable ingress
+
+docker build /storage/gitRepos/pori_ipr_api/ -t bcgsc/pori-ipr-api:uofc
+docker build -f /storage/gitRepos/pori/demo/Dockerfile.auth /storage/gitRepos/pori/ -t pori-keycloak
