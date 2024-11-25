3d2ae0eed169f392eb72f0531655c9f33fcbad95
Matthew Stuart-Edwards <matthew.stuartedward@ucalgary.ca>
2024-11-25 10:11:36 -0700

    Bootstrap the IPR database if empty
    If the IPR database is new, then it needs to be bootstrapped with users and data.  This is accomplished by putting the .dump file into the persistent storage and loading that.
    
    Additionally reorganized the kubernetes .yaml files into directories indicating their usages.  This makes it harder to load all the files with kubectl, but the organization is helping with development.

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -9,3 +9,13 @@ minikube addons enable ingress
 
 docker build /storage/gitRepos/pori_ipr_api/ -t bcgsc/pori-ipr-api:uofc
 docker build -f /storage/gitRepos/pori/demo/Dockerfile.auth /storage/gitRepos/pori/ -t pori-keycloak
+docker build -f /storage/gitRepos/pori_graphkb_loader/Dockerfile.snakemake -t bcgsc/pori-graphkb-loader:latest /storage/gitRepos/pori_graphkb_loader/
+
+export IPR_SERVICE_PASSWORD=root
+export IPR_SERVICE_USER=ipr_ro
+export IPR_GRAPHKB_PASSWORD=ipr_graphkb_link
+export TEMPLATE_NAME=PORI
+export TEMP_DB_NAME=temp_db
+export DB_DUMP_LOCATION=/tmp/
+export DATABASE_HOSTNAME=db.ipr.svc.cluster.local
+export CURR_TEMPLATE=template
